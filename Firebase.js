2cfb4f08386c31840dbbe3da8b6d6c017b4a21b4
Piyumal Karunathilaka <piyuk.thilaka@gmail.com>
2020-07-22 10:30:35 +0530

    Initialized repository

---
+++ Firebase.js
@@ -0,0 +1,73 @@
+import firebase from 'firebase'
+
+class Firebase {
+    constructor() {
+        this.init()
+        this.checkAuth()
+    }
+
+    init = () => {
+        if(!firebase.apps.length) {
+            firebase.initializeApp({
+                apiKey: "AIzaSyDQh_hRn0crczLdXdfdl5aCXnu_Jv0JF0g",
+                authDomain: "chatapp-3a71c.firebaseapp.com",
+                databaseURL: "https://chatapp-3a71c.firebaseio.com",
+                projectId: "chatapp-3a71c",
+                storageBucket: "chatapp-3a71c.appspot.com",
+                messagingSenderId: "1053255271333",
+                appId: "1:1053255271333:web:f7eaaadbe7d8aaea485e4c",
+                measurementId: "G-P9S9076TWJ"
+            });
+        }
+    };
+
+    checkAuth = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            if(!user) {
+                firebase.auth().signInAnonymously();
+            }
+        });
+    };
+
+    send = messages => {
+        messages.forEach(item => {
+            const message = {
+                text: item.text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            };
+            this.db.push(message);
+        });
+    };
+
+    parse = message => {
+        const { user, text, timestamp } = message.val();
+        const { key: _id } = message;
+        const createdAt = new Date(timestamp);
+
+        return {
+            _id,
+            createdAt,
+            text,
+            user
+        }
+    }
+
+    get = callback => {
+        this.db.on("child_added", snapshot => callback(this.parse(snapshot)));
+    }
+
+    off() {
+        this.db.off()
+    }
+
+    get db() {
+        return firebase.database().ref("messages");
+    }
+
+    get uid() {
+        return (firebase.auth().currentUser || {}).uid
+    }
+}
+
+export default new Firebase();
---
+++ screens/LoginScreen.js
@@ -0,0 +1,100 @@
+import React from 'react'
+import { Text, View, StyleSheet, TextInput, TouchableOpacity, Image } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
+// import Icon from 'react-native-vector-icons/Ionicons'
+
+import { YellowBox } from 'react-native';
+import _ from 'lodash';
+
+YellowBox.ignoreWarnings(['Setting a timer']);
+const _console = _.clone(console);
+console.warn = message => {
+  if (message.indexOf('Setting a timer') <= -1) {
+    _console.warn(message);
+  }
+};
+
+export default class LoginScreen extends React.Component {
+    state = {
+        name: ""
+    }
+
+    continue = () => {
+        this.props.navigation.navigate("Chat", {name: this.state.name})
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.circle} />
+                <View style={{ marginTop: 64 }}>
+                    <Image style={styles.image} source={require("../assets/chat.png")} />
+                </View>
+                <View style={{ marginHorizontal: 32 }}>
+                    <Text style={styles.header}>Username</Text>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Enter your name"
+                        onChangeText={name => {
+                            this.setState({name})}
+                        }
+                        value={this.state.name}
+                    />
+                    <View style={{ alignItems: "flex-end", marginTop: 64 }}>
+                        <TouchableOpacity style={styles.continue} onPress={this.continue}>
+                            <Ionicons name="md-arrow-round-forward" size={24} color="#FFF" />
+                            {/* <Icon name="ios-redo" style={styles.icon} /> */}
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#F4F5F7"
+    },
+    circle: {
+        width: 500,
+        height: 500,
+        borderRadius: 500 / 2,
+        backgroundColor: "#FFF",
+        position: "absolute",
+        left: -120,
+        top: -20
+    },
+    image: {
+        height: 100,
+        width: 100,
+        alignSelf: "center"
+    },
+    header: {
+        fontWeight: "800",
+        fontSize: 30,
+        color: "#514E5A",
+        marginTop: 32
+    },
+    input: {
+        marginTop: 32,
+        height: 50,
+        borderWidth: StyleSheet.hairlineWidth,
+        borderColor: "#BAB7C3",
+        borderRadius: 30,
+        paddingHorizontal: 16,
+        fontWeight: "600"
+    },
+    continue: {
+        width: 70,
+        height: 70,
+        borderWidth: 70 /2,
+        backgroundColor: "#9075E3",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    icon: {
+        color: "#FFF"
+    }
+});
